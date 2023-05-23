c722cc9b99386b05dad10a474b5ec8dcf18abb3b
achobanov <alexchobanov@gmail.com>
2023-05-23 18:49:18 +0300

    Open sounds panel on switch to fix sound devices
    ThinkVision sound devices glitch when connected to the laptop
    and opening Sounds panel turns it on and fixes missing sound

--- pc-switch/input-switch-primary.bat
+++ pc-switch/input-switch-primary.bat
@@ -2,3 +2,4 @@ cd C:\Program Files\ControlMyMonitor\
 ControlMyMonitor.exe /SetValueIfNeeded "LEN P24h-20" 60 15
 ControlMyMonitor.exe /SetValueIfNeeded "LEN T22i-10" 60 15
 start ms-settings:display
+control mmsys.cpl sounds
