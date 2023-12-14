87caa0f1f12a9b55698828668749c0a904b4f69e
achobanov <alexchobanov@gmail.com>
2023-12-14 13:45:16 +0200

    Integrate Y27q-30 as primary monitor

--- pc-switch/input-switch-primary.bat
+++ pc-switch/input-switch-primary.bat
@@ -1,5 +1,5 @@
 cd C:\environment\pc-switch
-ControlMyMonitor.exe /SetValueIfNeeded "LEN P24h-20" 60 15
+ControlMyMonitor.exe /SetValueIfNeeded "U3V04TF5" 60 15
 ControlMyMonitor.exe /SetValueIfNeeded "U3V04TF8" 60 15
-start ms-settings:display
+start ms-settings:display 
 control mmsys.cpl sounds
--- pc-switch/input-switch-secondary.bat
+++ pc-switch/input-switch-secondary.bat
@@ -1,4 +1,4 @@
 cd C:\environment\pc-switch
-ControlMyMonitor.exe /SetValueIfNeeded "LEN P24h-20" 60 19
+ControlMyMonitor.exe /SetValueIfNeeded "U3V04TF5" 60 17
 ControlMyMonitor.exe /SetValueIfNeeded "U3V04TF8" 60 17
 start ms-settings:display
