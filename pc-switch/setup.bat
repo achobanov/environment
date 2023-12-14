0f3045c06d31a2bcdee53abdc6f43aa060d6e3dd
achobanov <alexchobanov@gmail.com>
2023-12-14 13:14:49 +0200

    Finalize setup script independant of specific directory
    - Create shortcut file for ahk script
    - Put it in Windows Startup directory

--- pc-switch/setup.bat
+++ pc-switch/setup.bat
@@ -1,1 +1,18 @@
-copy /b/v/y "%cd%\input-switch.ahk - Shortcut.lnk" "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+@REM NO SPACE around the = sign!!
+set script_location="%cd%\input-switch.ahk"
+set shortcut_location="%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\input-switch.ahk - Shortcut.lnk"
+
+@echo off
+
+set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
+
+echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
+echo sLinkFile = %shortcut_location% >> %SCRIPT%
+echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
+echo oLink.TargetPath = %script_location% >> %SCRIPT%
+echo oLink.WorkingDirectory = "%cd%" >> %SCRIPT%
+echo oLink.Save >> %SCRIPT%
+
+cscript /nologo %SCRIPT%
+
+del %SCRIPT%
