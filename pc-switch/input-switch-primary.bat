3d7f073682250a2a4bbb2ae562a924a03a711620
achobanov <alexchobanov@gmail.com>
2023-12-14 13:46:28 +0200

    Remove system sound and display popups
    Test to see if they are necessary with new Lenovo monitors. May revert
    if so

--- pc-switch/input-switch-primary.bat
+++ pc-switch/input-switch-primary.bat
@@ -1,5 +1,3 @@
 cd C:\environment\pc-switch
 ControlMyMonitor.exe /SetValueIfNeeded "U3V04TF5" 60 15
 ControlMyMonitor.exe /SetValueIfNeeded "U3V04TF8" 60 15
-start ms-settings:display 
-control mmsys.cpl sounds
--- pc-switch/input-switch-secondary.bat
+++ pc-switch/input-switch-secondary.bat
@@ -1,4 +1,3 @@
 cd C:\environment\pc-switch
 ControlMyMonitor.exe /SetValueIfNeeded "U3V04TF5" 60 17
 ControlMyMonitor.exe /SetValueIfNeeded "U3V04TF8" 60 17
-start ms-settings:display
