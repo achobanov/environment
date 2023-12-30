72021b600ecc71980aa3d42b47624ca04cd86e85
achobanov <alexchobanov@gmail.com>
2023-12-30 16:43:49 +0200

    Remove unused scripts

--- monitors/sleep-monitors.bat
+++
@@ -1,3 +0,0 @@
-cd C:\environment\pc-switch
-ControlMyMonitor.exe /SetValueIfNeeded "LEN P24h-20" D6 4
-ControlMyMonitor.exe /SetValueIfNeeded "LEN T22i-10" D6 4
--- monitors/wake-monitors.bat
+++
@@ -1,3 +0,0 @@
-cd C:\environment\pc-switch
-ControlMyMonitor.exe /SetValueIfNeeded "LEN P24h-20" D6 1
-ControlMyMonitor.exe /SetValueIfNeeded "LEN T22i-10" D6 1
