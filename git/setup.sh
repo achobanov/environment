9620114c1bf8005128078e2a637b82b3e3721bed
Alexander Chobanov <alexander.chobanov@egt-digital.com>
2024-01-05 17:21:24 +0200

    Remove account credentials from git/setup.sh
    Github and other SCMs dont allow storing credentials in cli configs for
    security purposes. In order to prevent enterting credentials on every
    write I've decided to use the git-for-windows integrated Credentials
    Manager as it supports all major platforms and is build-in (opt-in)
    during installation

--- git/setup.sh
+++ git/setup.sh
@@ -2,8 +2,5 @@
 git config --global pull.rebase true
 git config --global fetch.prune true
 git config --global diff.colorMoved zebra
-git config --global user.name "achobanov"
-git config --global user.email "alexchobanov@gmail.com"
 
 echo git setup complete
-
