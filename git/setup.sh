c6ffc992eaa4607e3d59b04c3e820a5099c07a87
achobanov <alexchobanov@gmail.com>
2024-01-05 16:37:54 +0200

    Update username in git setup.sh

--- git/setup.sh
+++ git/setup.sh
@@ -2,7 +2,7 @@
 git config --global pull.rebase true
 git config --global fetch.prune true
 git config --global diff.colorMoved zebra
-git config --global user.name "Alex Chobanov"
+git config --global user.name "achobanov"
 git config --global user.email "alexchobanov@gmail.com"
 
 echo git setup complete
