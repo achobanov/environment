e4823eccf8f33e9024a667fe533a61d77fcb47bd
achobanov <alexchobanov@gmail.com>
2024-01-25 14:33:00 +0200

    SetValueIfNeeded doesnt work with EGT workstation

--- monitors/input-switch-primary.bat
+++ monitors/input-switch-primary.bat
@@ -1,4 +1,4 @@
 cd "CMM"
 
-ControlMyMonitor.exe /SetValueIfNeeded "U3V04TF5" 60 15
-ControlMyMonitor.exe /SetValueIfNeeded "U3V04TF8" 60 15
+ControlMyMonitor.exe /SetValue "U3V04TF5" 60 15
+ControlMyMonitor.exe /SetValue "U3V04TF8" 60 15
