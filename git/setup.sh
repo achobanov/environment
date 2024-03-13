2d83e156776cd970ceacdc49f78ef10db76a0aec
Alexander Chobanov <alexander.chobanov@egt-digital.com>
2024-03-13 12:49:37 +0200

    Configure git to use credential manager

--- git/setup.sh
+++ git/setup.sh
@@ -2,5 +2,6 @@
 git config --global pull.rebase true
 git config --global fetch.prune true
 git config --global diff.colorMoved zebra
+git config --global credential.helper manager
 
 echo git setup complete
